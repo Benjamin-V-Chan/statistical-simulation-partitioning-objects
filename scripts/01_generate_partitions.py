7d7655320888b05e9b9df9671f13a290f8587f08
BenjaminChan <benjamin.v.chan@gmail.com>
2025-04-12 00:02:09 -0700

    01_generate_partitions script outline

---
+++ scripts/01_generate_partitions.py
@@ -0,0 +1,5 @@
+# Steps:
+#   1. Import generate_partition from partition_utils.py.
+#   2. Define simulation parameters (num_objects, alpha).
+#   3. Generate a partition using generate_partition.
+#   4. Print out the partition (each group on a separate line).
